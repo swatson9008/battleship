49ec089317da65d6c71c3c1f5184c6875717bb50
Summer <swatson9008@yahoo.com>
2023-03-17 14:25:54 -0400

    setting up boats

--- src/ship.js
+++ src/ship.js
@@ -1,13 +1,20 @@
+/* eslint-disable no-unused-vars */
 /* eslint-disable func-names */
 /* eslint-disable max-len */
-const shipFactory = function (name, length) {
+const shipFactory = function (name, length, hits) {
   const ship = {};
   ship.name = name;
   ship.length = length;
-  ship.hits = 0;
+  ship.hits = hits;
   ship.isSunk = function sunkC() { if (this.length === this.hits) { return this.true; } return this.false; };
 
   return ship;
 };
+
+const carrier = shipFactory('carrier', 5, 0);
+const battleShip = shipFactory('battleship', 4, 0);
+const destroyer = shipFactory('destroyer', 3, 0);
+const submarine = shipFactory('submarine', 3, 0);
+const patrol = shipFactory('patrol', 2, 0);
 
 module.exports = shipFactory;
