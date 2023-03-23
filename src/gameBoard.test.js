173a2630039bd90fbc0525b6f04c7f06e16ef477
Summer <swatson9008@yahoo.com>
2023-03-22 20:26:10 -0400

    added more tests and cleaned up the code

--- src/gameBoard.test.js
+++ src/gameBoard.test.js
@@ -3,14 +3,18 @@
 /* eslint-disable no-undef */
 /* eslint-disable new-cap */
 import gameBoard from './gameBoard';
-import { carrier } from './ship';
+
+const playerBoard = new gameBoard();
 
 test('displays the correct length', () => {
-  const playerBoard = new gameBoard();
   expect(playerBoard.gameB.length).toEqual(10);
 });
 
 test('displays the correct message', () => {
-  const playerBoard = new gameBoard();
   expect(playerBoard.placeShipH()).toBe(true);
+});
+
+test('testing for a false answer', () => {
+  playerBoard.currCordY = 9;
+  expect(playerBoard.placeShipV()).toBe(false);
 });
