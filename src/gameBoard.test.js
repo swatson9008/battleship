c6296bb5c022f4ab1bf282e0a402d260477f07ae
Summer <swatson9008@yahoo.com>
2023-03-30 09:52:46 -0400

    fixed a test error

--- src/gameBoard.test.js
+++ src/gameBoard.test.js
@@ -5,14 +5,15 @@
 import gameBoard from './gameBoard';
 
 const playerBoard = new gameBoard('playerOne');
+playerBoard.placeShipH(playerBoard.ships.carrier, 2, 2);
+playerBoard.placeShipV(playerBoard.ships.patrol, 1, 1);
 
 test('displays the correct length', () => {
   expect(playerBoard.gameB.length).toEqual(10);
 });
 
 test('places the ship objects in the board horizontally', () => {
-  playerBoard.placeShipH(playerBoard.ships.carrier, 0, 0);
-  expect(playerBoard.gameB[0][0]).toBe(playerBoard.ships.carrier);
+  expect(playerBoard.gameB[2][2]).toBe(playerBoard.ships.carrier);
 });
 
 test('testing for a false answer', () => {
@@ -21,12 +22,10 @@ test('testing for a false answer', () => {
 });
 
 test('places the ship objects in the board vertically', () => {
-  playerBoard.placeShipV(playerBoard.ships.patrol, 1, 1);
   expect(playerBoard.gameB[1][1]).toBe(playerBoard.ships.patrol);
 });
 
 test('testing if the miss count works when receiving an attack with empty coords', () => {
-  playerBoard.placeShipH(playerBoard.ships.patrol, 1, 1);
   playerBoard.receiveAttack(0, 0);
   expect(playerBoard.missCount[0]).toBe(0, 0);
 });
