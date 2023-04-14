83efa70f33b9f74351ab49c3a93535e92831bfec
Summer <swatson9008@yahoo.com>
2023-04-14 16:56:25 -0400

    created a function to make the html boards + game

---
+++ src/game.js
@@ -0,0 +1,11 @@
+/* eslint-disable new-cap */
+/* eslint-disable no-unused-vars */
+import gameBoard from './gameBoard';
+import {
+  hPlayer, comPlayer,
+} from './players';
+
+const turnCount = 0;
+
+const playerBoard = new gameBoard('playerOne');
+const aiBoard = new gameBoard('playerTwo');
--- src/index.js
+++ src/index.js
@@ -1,1 +1,24 @@
+/* eslint-disable prefer-const */
+/* eslint-disable no-plusplus */
+/* eslint-disable no-unused-vars */
 import './style.css';
+
+const playerBField = document.getElementById('playerB');
+
+const aiBField = document.getElementById('aiB');
+
+function createplayerBoard(boardField) {
+  let rowId = 0;
+  for (let i = 0; i < 100; i++) {
+    let columnId = i % 10;
+    const newItem = document.createElement('div');
+    newItem.id = `${rowId}${columnId}`;
+    newItem.classList.add('boardSpace');
+    boardField.appendChild(newItem);
+    if (columnId === 9) { rowId++; }
+  }
+}
+
+createplayerBoard(playerBField);
+
+createplayerBoard(aiBField);
