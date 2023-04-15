5eb2406b605a38be4ad5c819049419ccf5a847e1
Summer <swatson9008@yahoo.com>
2023-04-14 20:05:42 -0400

    assigned predetermined coordinates

--- src/game.js
+++ src/game.js
@@ -14,4 +14,17 @@ const aiPlayer = comPlayer();
 const humanPlayer = hPlayer();
 
 playerBoard.placeShipH(playerBoard.ships.carrier, 1, 1);
+playerBoard.placeShipV(playerBoard.ships.battleship, 2, 0);
+playerBoard.placeShipH(playerBoard.ships.destroyer, 5, 5);
+playerBoard.placeShipV(playerBoard.ships.submarine, 7, 3);
+playerBoard.placeShipH(playerBoard.ships.patrol, 0, 6);
 
+aiBoard.placeShipH(aiBoard.ships.patrol, 9, 1);
+aiBoard.placeShipV(aiBoard.ships.submarine, 1, 9);
+aiBoard.placeShipH(aiBoard.ships.destroyer, 0, 0);
+aiBoard.placeShipV(aiBoard.ships.battleship, 2, 4);
+aiBoard.placeShipH(aiBoard.ships.carrier, 7, 2);
+
+function startGame() {
+
+}
