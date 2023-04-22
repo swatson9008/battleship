18e0292a7bf7bf7ba271c2130c4d5f909bef30c1
Summer <swatson9008@yahoo.com>
2023-04-22 19:53:02 -0400

    cleaned code

--- src/game.js
+++ src/game.js
@@ -1,11 +1,20 @@
+/* eslint-disable import/no-cycle */
+/* eslint-disable no-alert */
+/* eslint-disable no-const-assign */
+/* eslint-disable consistent-return */
+/* eslint-disable radix */
+/* eslint-disable no-plusplus */
+/* eslint-disable no-param-reassign */
+/* eslint-disable no-undef */
 /* eslint-disable new-cap */
 /* eslint-disable no-unused-vars */
 import gameBoard from './gameBoard';
 import {
   hPlayer, comPlayer,
 } from './players';
+import { playerGroup, playerCells, boardCells } from '.';
 
-const turnCount = 0;
+let turnCount = 0;
 
 const playerBoard = new gameBoard('playerOne');
 const aiBoard = new gameBoard('playerTwo');
@@ -13,18 +22,44 @@ const aiBoard = new gameBoard('playerTwo');
 const aiPlayer = comPlayer();
 const humanPlayer = hPlayer();
 
-playerBoard.placeShipH(playerBoard.ships.carrier, 1, 1);
-playerBoard.placeShipV(playerBoard.ships.battleship, 2, 0);
-playerBoard.placeShipH(playerBoard.ships.destroyer, 5, 5);
-playerBoard.placeShipV(playerBoard.ships.submarine, 7, 3);
-playerBoard.placeShipH(playerBoard.ships.patrol, 0, 6);
+let computerAttempts = 0;
 
-aiBoard.placeShipH(aiBoard.ships.patrol, 9, 1);
-aiBoard.placeShipV(aiBoard.ships.submarine, 1, 9);
-aiBoard.placeShipH(aiBoard.ships.destroyer, 0, 0);
-aiBoard.placeShipV(aiBoard.ships.battleship, 2, 4);
-aiBoard.placeShipH(aiBoard.ships.carrier, 7, 2);
-
-function startGame() {
-
+function computerTurn(board) {
+  if (aiPlayer.randomAttack(board, 0, 9, 0, 9) === 'hit') {
+    playerCells.forEach((div) => {
+      if (div.id === String(aiPlayer.attackCord[computerAttempts])) {
+        div.style.backgroundColor = '#F07B7B';
+        div.textContent = 'X';
+      }
+    });
+    if (playerBoard.reportAllSunk() === true) { alert('Computer Wins'); } else {
+      computerAttempts++;
+      computerTurn(board);
+    }
+  } else {
+    playerCells.forEach((div) => {
+      if (div.id === String(aiPlayer.attackCord[computerAttempts])) {
+        div.textContent = 'X';
+      }
+    });
+    computerAttempts++;
+  }
 }
+
+function playersTurn(element, board) {
+  if (element.textContent === 'X') { alert('cannot be selected'); } else if (board.gameB[parseInt(element.id.charAt(0))][parseInt(element.id.charAt(1))] === null) {
+    element.textContent = 'X';
+    turnCount++;
+    humanPlayer.chooseAttack(board, parseInt(element.id.charAt(0)), parseInt(element.id.charAt(1)));
+    computerTurn(playerBoard);
+  } else {
+    humanPlayer.chooseAttack(board, parseInt(element.id.charAt(0)), parseInt(element.id.charAt(1)));
+    element.style.backgroundColor = '#F07B7B';
+    element.textContent = 'X';
+    if (aiBoard.reportAllSunk() === true) { return alert('You win'); } alert('hit');
+  }
+}
+
+export {
+  playersTurn, computerTurn, aiPlayer, humanPlayer, aiBoard, playerBoard,
+};
--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable import/no-cycle */
 /* eslint-disable no-unused-expressions */
 /* eslint-disable no-param-reassign */
 /* eslint-disable consistent-return */
@@ -17,9 +18,11 @@ import gameBoard from './gameBoard';
 import {
   hPlayer, comPlayer,
 } from './players';
+import {
+  aiPlayer, humanPlayer, aiBoard, playerBoard, computerTurn, playersTurn,
+} from './game';
 
 const playerBField = document.getElementById('playerB');
-
 const aiBField = document.getElementById('aiB');
 
 function createplayerBoard(boardField) {
@@ -38,25 +41,11 @@ createplayerBoard(aiBField);
 
 const boardCells = document.querySelectorAll('.boardSpace');
 
-let playerBoard = new gameBoard('playerOne');
-let aiBoard = new gameBoard('playerTwo');
-let aiPlayer = comPlayer(playerBoard);
-let humanPlayer = hPlayer(aiBoard);
-
-let turnCount = 0;
-let computerAttempts = 0;
-
 playerBoard.placeShipH(playerBoard.ships.carrier, 1, 1);
 playerBoard.placeShipV(playerBoard.ships.battleship, 2, 0);
 playerBoard.placeShipH(playerBoard.ships.destroyer, 5, 5);
 playerBoard.placeShipV(playerBoard.ships.submarine, 7, 3);
 playerBoard.placeShipH(playerBoard.ships.patrol, 0, 6);
-
-// aiBoard.placeShipH(aiBoard.ships.patrol, 9, 1);
-// aiBoard.placeShipV(aiBoard.ships.submarine, 9, 9);
-// aiBoard.placeShipH(aiBoard.ships.destroyer, 0, 0);
-// aiBoard.placeShipV(aiBoard.ships.battleship, 2, 4);
-// aiBoard.placeShipH(aiBoard.ships.carrier, 7, 2);
 
 aiPlayer.randomSelection(aiBoard, aiBoard.ships.carrier);
 aiPlayer.randomSelection(aiBoard, aiBoard.ships.battleship);
@@ -71,44 +60,10 @@ const playerCells = playerGroup.querySelectorAll('div');
 
 console.log(aiBoard.gameB);
 
-function computerTurn(board) {
-  if (aiPlayer.randomAttack(board, 0, 9, 0, 9) === 'hit') {
-    playerCells.forEach((div) => {
-      if (div.id === String(aiPlayer.attackCord[computerAttempts])) {
-        div.style.backgroundColor = '#F07B7B';
-        div.textContent = 'X';
-      }
-    });
-    if (playerBoard.reportAllSunk() === true) { alert('Computer Wins'); } else {
-      computerAttempts++;
-      computerTurn(board);
-    }
-  } else {
-    playerCells.forEach((div) => {
-      if (div.id === String(aiPlayer.attackCord[computerAttempts])) {
-        div.textContent = 'X';
-      }
-    });
-    computerAttempts++;
-  }
-}
-
-function playersTurn(element, board) {
-  if (element.textContent === 'X') { alert('cannot be selected'); } else if (board.gameB[parseInt(element.id.charAt(0))][parseInt(element.id.charAt(1))] === null) {
-    element.textContent = 'X';
-    turnCount++;
-    humanPlayer.chooseAttack(board, parseInt(element.id.charAt(0)), parseInt(element.id.charAt(1)));
-    computerTurn(playerBoard);
-  } else {
-    humanPlayer.chooseAttack(board, parseInt(element.id.charAt(0)), parseInt(element.id.charAt(1)));
-    element.style.backgroundColor = '#F07B7B';
-    element.textContent = 'X';
-    if (aiBoard.reportAllSunk() === true) { return alert('You win'); } alert('hit');
-  }
-}
-
 boardCells.forEach((div) => {
   div.addEventListener('click', () => {
     playersTurn(div, aiBoard);
   });
 });
+
+export { playerGroup, playerCells, boardCells };
