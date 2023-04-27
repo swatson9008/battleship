f40b31ace668049227a60f9e00a98b1f14a827f6
Summer <swatson9008@yahoo.com>
2023-04-27 17:20:34 -0400

    working on manual ship placement

--- main.js
+++ main.js
@@ -269,6 +269,8 @@ const aiBField = document.getElementById('aiB');
 const gameStart = document.getElementById('gameStart');
 const playerControls = document.getElementById('playerControl');
 const randomButton = document.getElementById('randomize');
+const shipSwap = document.getElementById('shipHV');
+let orientationSetting = 'horizontal';
 function startGame() {
   aiBField.style.visibility = 'visible';
   gameStart.style.visibility = 'hidden';
@@ -326,6 +328,50 @@ randomButton.addEventListener('click', () => {
   console.log(_game__WEBPACK_IMPORTED_MODULE_3__.playerBoard.gameB);
   gameStart.style.visibility = 'visible';
   paintCells(_game__WEBPACK_IMPORTED_MODULE_3__.playerBoard);
+});
+function switchOrientation() {
+  if (orientationSetting === 'horizontal') {
+    orientationSetting = 'vertical';
+  } else {
+    orientationSetting = 'horizontal';
+  }
+  console.log(orientationSetting);
+}
+shipSwap.addEventListener('click', () => {
+  switchOrientation();
+});
+function determineShip(board) {
+  let nullCount = 0;
+  for (let i = 0; i < board.gameB.length; i++) {
+    for (let j = 0; j < board.gameB[i].length; j++) {
+      if (board.gameB[i][j] !== null) {
+        nullCount++;
+      }
+    }
+  }
+  if (nullCount === 0) {
+    return board.ships.carrier;
+  }
+  if (nullCount === 5) {
+    return board.ships.battleship;
+  }
+  if (nullCount === 9) {
+    return board.ships.destroyer;
+  }
+  if (nullCount === 12) {
+    return board.ships.submarine;
+  }
+  if (nullCount === 15) {
+    return board.ships.patrol;
+  }
+  if (nullCount === 17) {
+    return 'done';
+  }
+}
+playerCells.forEach(cell => {
+  cell.addEventListener('click', () => {
+    determineShip(_game__WEBPACK_IMPORTED_MODULE_3__.playerBoard);
+  });
 });
 
 
